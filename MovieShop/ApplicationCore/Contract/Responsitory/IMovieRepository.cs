c86a52e5d80f48d28e9aaeb645f9094c38629ea5
Ruihan Chen <ruihanchen@berkeley.edu>
2022-06-14 17:36:35 -0400

    Add files via upload

---
+++ MovieShop/ApplicationCore/Contract/Responsitory/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contract.Responsitary
+{
+    public interface IMovieRepository
+    {
+        //all the database logic methods for movies
+
+        //GetMovie(int Id)
+    }
+}
